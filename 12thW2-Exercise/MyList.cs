c93a5338849ca93036cca0d55d8f14498ebcc1c8
Orlin_ <orlin221@abv.bg>
2023-05-01 08:32:48 +0300

    Indexer using
    Exercises

---
+++ 12thW2-Exercise/MyList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12thW2_Exercise
+{
+    public class MyList
+    {
+		
+        private List<int> lists = new List<int>();
+		// Declarating Indexer,elements
+
+		//We can use indexer to set values to the elements, which easy and very good way!
+		public int this[int index]
+		{
+			// get and set accessors	
+       		get { return lists[ index]; }
+			set { lists[index] = value; }
+		}
+
+		public void Add(int item)
+		{
+			lists.Add(item);
+		}
+		public void Remove(int item)
+		{
+			lists.Remove(item);
+		}
+	}
+}
---
+++ 12thW2-Exercise/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.AccessControl;
+using System.Xml.Linq;
+
+namespace _12thW2_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a MyList class that will work exactly
+            //like a List class, but only for integers.
+            //You must create Add and Remove methods,
+            //and this element must also be accessible
+            //through an Indexer of your class.
+            List<int> myLists = new List<int>() {2,3,4,5};
+            
+            myLists.Add(6);
+            myLists.Remove(1);
+            myLists[0] = 1;
+            myLists[4] = 7;
+            foreach (var m in myLists)
+            {
+                Console.WriteLine(m);
+            }
+  
+        }
+    }
+}
